299cf005c9dd51dc7df098554ae404126a05cb17
KOSASIH <kosasihg88@gmail.com>
2024-10-21 12:57:25 +0700

    Create Footer.js

---
+++ src/frontend/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './components.scss';
+
+const Footer = () => {
+    return (
+        <footer className="app-footer">
+            <p>&copy; 2023 AquaGuard Alliance. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
