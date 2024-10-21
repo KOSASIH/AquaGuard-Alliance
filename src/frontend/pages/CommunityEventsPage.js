1d1dbcaa3b7d3647413c7da0d11866018b3ae4a7
KOSASIH <kosasihg88@gmail.com>
2024-10-21 12:59:56 +0700

    Create CommunityEventsPage.js

---
+++ src/frontend/pages/CommunityEventsPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const CommunityEventsPage = () => {
+    return (
+        <div>
+            <Header />
+            <main>
+                <h2>Community Events</h2>
+                <p>List of upcoming community events will be displayed here.</p>
+            </main>
+            <Footer />
+        </div>
+    );
+};
+
+export default CommunityEventsPage;
